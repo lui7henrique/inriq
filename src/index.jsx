6eeee9ead3ccda5d59c4d943dffe9f9739504ef5
Luiz Henrique <7henrique18@gmail.com>
2023-09-21 20:53:06 -0300

    first commit

---
+++ src/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import { createRoot } from 'react-dom/client'
+import './styles.css'
+import { App } from './App'
+import { Leva } from 'leva'
+
+createRoot(document.getElementById('root')).render(
+  <>
+    <App />
+    <Leva collapsed />
+  </>,
+)
